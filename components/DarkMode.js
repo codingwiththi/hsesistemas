fcaaff143488f58bdc849e1cb1b359a4eb058cc6
Thiago Moreira <39631037+thiagomr8@users.noreply.github.com>
2023-01-11 00:09:27 -0300

    init

---
+++ components/DarkMode.js
@@ -0,0 +1,74 @@
+import React, { useEffect, useState } from "react";
+
+function DarkModeToggle() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  useEffect(() => {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      document.documentElement.classList.add("dark");
+      setDarkMode(true);
+    } else {
+      document.documentElement.classList.remove("dark");
+      setDarkMode(false);
+    }
+  }, []);
+
+  const toggleDarkMode = () => {
+    if (darkMode) {
+      localStorage.theme = "light";
+      document.documentElement.classList.remove("dark");
+      setDarkMode(!darkMode);
+    } else {
+      localStorage.theme = "dark";
+      document.documentElement.classList.add("dark");
+      setDarkMode(!darkMode);
+    }
+  };
+
+  return (
+    <div className="absolute bottom-0 right-0 m-4">
+      <button
+        onClick={toggleDarkMode}
+        className={`flex items-center   font-medium  rounded-full p-2 shadow-lg focus:outline-none focus:ring-2  focus:ring-opacity-50 transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-110 dark:text-yellow-600 dark:bg-white  dark:hover:text-yellow-800 dark:focus:ring-yellow-900 text-gray-100 bg-gray-800 hover:text-gray-300 focus:ring-indigo-600`}
+      >
+        {darkMode ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+            />
+          </svg>
+        )}
+      </button>
+    </div>
+  );
+}
+
+export default DarkModeToggle;
