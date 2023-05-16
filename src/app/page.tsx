eb9d40e6ca5b116fb75fa6a6c8ad38845ef5d5cb
Thiago Moreira <thiago.moreira.ribeiro@gmail.com>
2023-05-16 00:44:56 -0300

    navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import Navbar from "@/components/navbar";
 import AboutMe from "@/components/about";
 import Hero from "@/components/hero";
 import SystemSection from "@/components/SystemSection";
 import Clients from "@/components/Clients";
 import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
+import Navbar from "@/components/Navbar";
 
 export default function Home() {
   return (
